c22d99991db7e13ca85d52130bd27c40403f6e60
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2023-06-03 12:37:02 +0200

    Update Blazor MAUI version and restructure project

---
+++ TC2.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TC2.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ TC2.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TC2.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
